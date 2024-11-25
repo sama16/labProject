e43265350e3f57e4b612f15fe2bdd5cbdff5459a
AliaRashed1 <aliarashed74@aucegypt.edu>
2024-11-25 19:54:36 +0200

    Update adminview.h

--- LMS-main/adminview.h
+++ LMS-main/adminview.h
@@ -24,32 +24,30 @@ private:
     void saveSeminars();
 
 
-    // Book management
+   
     QLineEdit *bookTitleInput;
     QLineEdit *bookAuthorInput;
-    QStringList bookList; // Store book information
+    QStringList bookList; 
 
-    // User management
+    
     QLineEdit *userInput;
-    QStringList userList; // Store usernames
+    QStringList userList; 
      QListWidget *listView;
-    //seminar management
+    
     QLineEdit *seminarTitleInput;
     QLineEdit *seminarDateInput;
       QStringList seminarList;
 
 
-    // Book functions
+    
     void addBook();
     void editBook();
     void deleteBook();
 
-    // User functions
     void addUser();
     void deleteUser();
     void manageUsers();
 
-    //seminar finction
     void addSeminar();
     void editSeminar();
     void deleteSeminar();
